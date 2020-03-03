cd54a45e17e66f9d26adbcebb6d2fbf34f3f3a6a
Ivan <Ivanpuzako@gmail.com>
2020-03-03 16:34:12 +0300

    changed

--- task1/obj_detection.py
+++ task1/obj_detection.py
@@ -8,10 +8,6 @@ import cv2 as cv
 from cats import cats
 from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
 
-
-
-
-
 parser = argparse.ArgumentParser(description='Add path to photo')
 parser.add_argument('source')
 parser.add_argument('dest')
@@ -23,7 +19,6 @@ model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
 pretrained =True
 num_classes = 81
 in_features = model.roi_heads.box_predictor.cls_score.in_features
-
 transform = torchvision.transforms.ToTensor()
 transformed = transform(Img).unsqueeze(0)
 model.eval()
@@ -43,6 +38,6 @@ def show_result(img, result, threshold = 0.7):
             cv.rectangle(image, (box[0], box[1]), (box[2], box[3]), (0,255,0), 4)
             cv.putText(image, cats[int(label)], (int(box[0]), int(box[1])-10), cv.FONT_HERSHEY_SIMPLEX, 1, (255,0,0), 3)
     return image
-output_image = cv.cvtColor(show_result(img, result), cv.COLOR_RGB2BGR)   
 
+output_image = cv.cvtColor(show_result(img, result), cv.COLOR_RGB2BGR)   
 cv.imwrite(dest, output_image)
